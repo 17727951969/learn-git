848ea12780e7c5e1dbdbf1f06f1cf5e310d947a1
朱仁炜 <471016982@qq.com>
2021-03-07 16:02:57 +0800

    1

--- src/main/java/CaseInsensitiveString.java
+++ src/main/java/CaseInsensitiveString.java
@@ -29,7 +29,7 @@ public final class CaseInsensitiveString {
         list.add(cis);
 
         System.out.println(list.contains(s))
-    }
+
 
 //    // Fixed equals method (Page 40)
 //    @Override public boolean equals(Object o) {
